7874d5abc4cf28afc25d3f983054f2102c8bfc0d
Harry Moreno <harry@hayden5.com>
2022-10-18 18:07:20 -0400

    first

---
+++ django_2022/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_2022.users.models import User
+from django_2022.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
